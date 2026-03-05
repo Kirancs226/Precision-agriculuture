31980f2c60a5a55cf717a6e4340e7aafc12db6b2
kirancs226 <kirancs226@gmail.com>
2026-03-05 20:13:40 +0530

    Added crop dataset and tarining script

---
+++ models/train_crop_model.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+
+# Load dataset
+data = pd.read_csv("datasets/Crop_recommendation.csv")
+
+# Split features and labels
+X = data.drop("label", axis=1)
+y = data["label"]
+
+# Train/test split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Train model
+model = RandomForestClassifier()
+
+model.fit(X_train, y_train)
+
+# Save model
+pickle.dump(model, open("crop_model.pkl", "wb"))
+
+print("Crop model trained successfully")
