36a88fe1777ef5b9ae52afac394e9da8354ae197
kirancs226 <kirancs226@gmail.com>
2026-03-05 18:54:58 +0530

    Added weather API inegration

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from services.weather_service import get_weather
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    weather = get_weather("Chennai")
+    return render_template("index.html", weather=weather)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ services/weather_service.py
@@ -0,0 +1,18 @@
+import requests
+
+API_KEY = "e400c90e1111ea8ebee4ba8ed40e9b73"
+
+def get_weather(city):
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+
+    response = requests.get(url)
+    data = response.json()
+
+    weather = {
+        "temperature": data["main"]["temp"],
+        "humidity": data["main"]["humidity"],
+        "wind_speed": data["wind"]["speed"],
+        "description": data["weather"][0]["description"]
+    }
+
+    return weather
